fa962fbd501ca6cdbcd932315c84b33b9050f5bc
Clay Dowling <clay@lazarusid.com>
2020-04-28 21:42:20 -0400

    First files, and bringing in yaml

---
+++ experienceevent.h
@@ -0,0 +1,14 @@
+#ifndef _EXPERIENCEEVENT_H_
+#define _EXPERIENCEEVENT_H_
+
+#include <string>
+
+using namespace std;
+
+struct {
+    string character;
+    unsigned int points;
+    string reason;
+};
+
+#endif
