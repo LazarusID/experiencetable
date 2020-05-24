8e4f8229bb942c858a287b9f998b5717a4faf5d9
Clay Dowling <clay@lazarusid.com>
2020-05-23 20:51:29 -0400

    Trying to drive tests out

--- test/test_gameday.cpp
+++ test/test_gameday.cpp
@@ -2,28 +2,27 @@
 // Created by clay on 5/17/20.
 //
 
-#include <gtest/gtest.h>
 #include "gameday.h"
+#include <gtest/gtest.h>
 
 TEST(gameday_suite, identical_sessions_are_equal) {
 
-    gameday left;
-    gameday right;
+  gameday left;
+  gameday right;
 
-    left.session = "You're in a tavern";
-    right.session = "You're in a tavern";
+  left.session = "You're in a tavern";
+  right.session = "You're in a tavern";
 
-    ASSERT_TRUE(left == right);
+  ASSERT_TRUE(left == right);
 }
 
 TEST(gameday_suite, non_matching_sessions_are_not_equal) {
 
-    gameday left;
-    gameday right;
+  gameday left;
+  gameday right;
 
-    left.session = "You're in a tavern";
-    right.session = "Roll for initiative";
+  left.session = "You're in a tavern";
+  right.session = "Roll for initiative";
 
-    ASSERT_FALSE(left == right);
+  ASSERT_FALSE(left == right);
 }
-
