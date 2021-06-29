c5ba555cfc6b6a2d60f9005758d681873a923e88
Clay Dowling <clay@lazarusid.com>
2021-06-28 22:25:48 -0400

    Updated documentation and examples
    New format is now reflected in documentation and examples.
    
    Not all tests pass yet, made a failing test for what to do when input
    ends.

--- test/test_experienceevent.cpp
+++ test/test_experienceevent.cpp
@@ -7,54 +7,57 @@
 using namespace std;
 
 int main(void) {
-    describe("Plain Text ExperienceEvent", [] {
+  describe("Plain Text ExperienceEvent", [] {
+    it("should populate event and leave reason empty when reading from input "
+       "stream",
+       [] {
+         stringstream src("Ron 7");
+         experienceevent e;
 
-        it("should populate event and leave reason empty when reading from input stream", [] {
-            stringstream src("Ron 7");
-            experienceevent e;
+         src >> e;
 
-            src >> e;
+         expect(e.character).toBe(string("Ron"));
+         expect(e.points).toBe(7);
+         expect(e.reason).toBe(string(""));
+       });
 
-            expect(e.character).toBe(string("Ron"));
-            expect(e.points).toBe(7);
-            expect(e.reason).toBe(string(""));
-        });
+    it("should populate reason if reason is present", [] {
+      stringstream src("Ron 6 Because of awesomeness");
+      experienceevent e;
 
-        it("should populate reason if reason is present", [] {
-            stringstream src("Ron 6 Because of awesomeness");
-            experienceevent e;
+      src >> e;
 
-            src >> e;
-
-            expect(e.character).toBe(string("Ron"));
-            expect(e.points).toBe(6);
-            expect(e.reason).toBe(string("Because of awesomeness"));
-        });
-
-        it("should leave values in place if stream has ended", [] {
-            stringstream src;
-            experienceevent e("Harry", 3, "Because he's harry");
-
-            src >> e;
-
-            expect(e.character).toBe(string("Harry"));
-            expect(e.points).toBe(3);
-            expect(e.reason).toBe(string("Because he's harry"));
-        });
+      expect(e.character).toBe(string("Ron"));
+      expect(e.points).toBe(6);
+      expect(e.reason).toBe(string("Because of awesomeness"));
     });
 
-    describe("CSV ExperienceEvent", [] {
-        it("Populates all fields", [] {
-            stringstream src("\"Harry\",3,\"General Awesomeness\"");
-            experienceevent e;
+    it("should set default values if stream has ended", [] {
+      stringstream src;
+      experienceevent e("Harry", 3, "Because he's harry");
 
-            src >> e;
+      src >> e;
 
-            expect(e.character).toBe(string("Harry"));
-            expect(e.points).toBe(3);
-            expect(e.reason).toBe(string("General Awesomeness"));
-        });
+      expect(e.character).toBe(string(""));
+      expect(e.points).toBe((int)0);
+      expect(e.reason).toBe(string(""));
     });
+  });
 
-    return failed;
+  xdescribe("CSV ExperienceEvent", [] {
+    it("Populates all fields", [] {
+      stringstream src("\"Harry\",3,\"General Awesomeness\"");
+      experienceevent e;
+
+      src >> e;
+
+      expect(e.character).toBe(string("Harry"));
+      expect(e.points).toBe(3);
+      expect(e.reason).toBe(string("General Awesomeness"));
+    });
+  });
+
+  testSummary();
+
+  return failed;
 }
